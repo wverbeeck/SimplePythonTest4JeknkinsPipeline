03738ef48809fc39478f1adb52a88c6572cb079a
wverbeeck <49304641+wverbeeck@users.noreply.github.com>
2022-07-11 15:19:53 +0200

    Create TestCode.py
    simple test code

---
+++ TestCode.py
@@ -0,0 +1,15 @@
+import os
+List1 = [8,9,3,6,1,10]
+List1.reverse()
+print("The reversed list is: ", List1)
+
+List2 = [91, 67, 120, 34, 76, 54, 78, 87, 56, 64, 345]
+List2.sort()
+print("The sorted list is: ", List2)
+
+List3 = []
+List3 = List1.copy()
+print("List3 = ", List3)
+
+indexvalue = List2[2:6]
+print("The indexvalues are ", indexvalue)
